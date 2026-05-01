a1fa47c9601da0b58671a6162d9a7ce93ad25de2
Hugonofre <hugoant7@gmail.com>
2026-04-30 22:34:22 -0600

    feat(gossip): add basic test for net-cluster-discovery

---
+++ projects/net-cluster-discovery/tests/test_gossip.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <sys/gossip_node.hpp>
+
+int main()
+{
+    std::cout << "=== Gossip Protocol Tests ===" << std::endl;
+    std::cout << "Tests coming soon..." << std::endl;
+    return 0;
+}
